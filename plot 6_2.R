702ed784d43189352664251e1630defc0f634ef2
reutyem <142226591+reutyem@users.noreply.github.com>
2023-08-16 11:35:00 +0300

    Add files via upload

--- plot 6_2.R
+++ plot 6_2.R
@@ -10,26 +10,23 @@ SCC <- readRDS("Source_Classification_Code.rds")
 
 merged_data <- NEI %>%
   left_join(SCC, by = "SCC")
-BaltimorelosData <- merged_data[merged_data$fips %in% c("24510","06037"), ]
+BaltimoreData <- merged_data[merged_data$fips == "24510", ]
 
-motor_vehicle_sources <- BaltimorelosData %>%
+motor_vehicle_sources <- BaltimoreData %>%
   filter(grepl("Vehicle", Short.Name))
 
 motor_vehicle_emissions <- motor_vehicle_sources %>%
-  group_by(fips, year) %>%
+  group_by(year) %>%
   summarise(total_emissions = sum(Emissions))
 
-png("plot6_2.png", width = 480, height = 480)
-
-ggplot(motor_vehicle_emissions, aes(x = year, y = total_emissions, fill = fips)) +
-  geom_bar(aes(fill = fips), position = "dodge", stat="identity") +
-  labs(x = "City",
+png("plot5_2.png", width = 480, height = 480)
+ggplot(motor_vehicle_emissions, aes(x = year, y = total_emissions)) +
+  geom_line(color = "blue") +
+  geom_point() +
+  labs(x = "Year",
        y = expression('Total PM'[2.5]*" Emission (in tons)"),
-       title = expression('Changes in PM'[2.5]*" Emissions from Motor Vehicle-Related Sources")) +
-  theme_minimal() +
-  scale_fill_manual(values = c("24510" = "24510", "06037" = "06037"),
-                    labels = c("24510" = "Baltimore", "06037" = "Los Angeles")) +
-  scale_x_discrete(labels = c("24510" = "Baltimore", "06037" = "Los Angeles")) +
-  guides(fill = guide_legend(title = "City"))
+       title =  expression('Changes in PM'[2.5]*" Emissions from Motor Vehicle-Related Sources")) +
+  theme_minimal()
+
 
 dev.off()
